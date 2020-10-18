79251a8f9cd3d0eb8bfb2e7b7e02e073e4687c06
rohitbakoliya <bakoliyarohit00@gmail.com>
2020-10-18 23:35:42 +0530

    manifest and config file updated

--- src/components/Utils/config.js
+++ src/components/Utils/config.js
@@ -3,8 +3,6 @@ if(process.env.NODE_ENV==="development"){
      config = {
           'client_id': process.env.REACT_APP_CLIENT_ID_LOCAL,
           'client_secret' : process.env.REACT_APP_CLIENT_SECRET_LOCAL,
-          'client_id' : '9e3ad50cd5399b9a6560595022689f7c',
-          'client_secret': 'fdfbd7e48968147750b2724a26a6186b',
           'api_endpoint': 'https://api.codechef.com/',
           'authorization_code_endpoint': 'https://api.codechef.com/oauth/authorize',
           'access_token_endpoint': 'https://api.codechef.com/oauth/token',
